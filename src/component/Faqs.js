d93bc092b44220ca8d0dbd38156a2ca5086b64be
Aakrti <bhalekaraakrti@gmail.com>
2024-11-07 23:48:36 +0530

    footer added

--- src/component/Faqs.js
+++ src/component/Faqs.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Accordion from 'react-bootstrap/Accordion';
-// import './Faq.css';
+import './Faqs.css';
 
 const Faqs = () => {
   return (
@@ -11,7 +11,7 @@ const Faqs = () => {
           <Accordion.Header className='faq-header'>
             Can I join Graphene InfoTech as a beginner?
           </Accordion.Header>
-          <Accordion.Body>
+          <Accordion.Body className='text-start'>
             Absolutely! Graphene Infotech offers programs suited for both
             beginners and advanced learners. Our curriculum is structured to
             help you build a strong foundation before moving on to more complex
@@ -22,7 +22,7 @@ const Faqs = () => {
           <Accordion.Header className='faq-header'>
             Who is eligible to join Graphene InfoTech Training Program?
           </Accordion.Header>
-          <Accordion.Body>
+          <Accordion.Body className='text-start'>
             Graphene Infotech courses are open to anyone interested in
             upskilling or starting a career in the digital domain. Whether
             you're a student, a working professional, or a freelancer, our
@@ -33,7 +33,7 @@ const Faqs = () => {
           <Accordion.Header className='faq-header'>
             Is it necessary to have an engineering background?
           </Accordion.Header>
-          <Accordion.Body>
+          <Accordion.Body className='text-start'>
             No, an engineering background is not necessary. Our courses are
             designed for learners from various educational backgrounds. Whether
             you’re new to tech or looking to upskill, we provide the
@@ -45,7 +45,7 @@ const Faqs = () => {
           <Accordion.Header className='faq-header'>
             Can I get help with mock interviews?
           </Accordion.Header>
-          <Accordion.Body>
+          <Accordion.Body className='text-start'>
             Yes, we offer dedicated support for mock interviews. Our mentors and
             career coaches will conduct practice interviews to help you prepare
             for real-world situations and enhance your performance.
@@ -55,7 +55,7 @@ const Faqs = () => {
           <Accordion.Header className='faq-header'>
             Does Graphene InfoTech provide certificates?
           </Accordion.Header>
-          <Accordion.Body>
+          <Accordion.Body className='text-start'>
             Upon successfully completing a course at Graphene Infotech, you’ll
             receive a certificate that verifies your newly gained skills. This
             certificate is a valuable asset to enhance your resume and LinkedIn
--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
+import { Image } from 'react-bootstrap';
+import logo from '../icons/giLogo.jpg';
+import './Footer.css';
+import A from '../icons/icons8-msg.png';
+import B from '../icons/icons8-call.png';
 
 function Footer() {
   return (
-    <footer className="border-stone-500 border-solid">
-      <div className="flex flex-col max-w-full justify-center items-center min-h-60 container mx-auto px-6 md:px-1 ">
+    <footer className='border-stone-500 border-solid'>
+      {/* <div className="flex flex-col max-w-full justify-center items-center min-h-60 container mx-auto px-6 md:px-1 ">
         <div className="flex flex-col">
           <div className="flex listing">
           <a href="#home" className="text-gray-500 hover:text-black px-4 py-2">Home</a>
@@ -31,9 +40,132 @@ function Footer() {
           <p>&copy; {new Date().getFullYear()} Graphene infoTech. All rights reserved.</p>
         </div>
         </div>
-      </div>
+      </div> */}
+      <div className='footer-block py-2 mt-5'>
+        <div className='px-5 ul'>
+          <div className=' py-4 d-flex justify-content-around'>
+            <div
+              style={{
+                flexDirection: 'column',
+              }}
+              className='my-5 col-lg-3 col-md-12 col-sm-12 d-flex justify-content-md-center footer-info'
+            >
+              <h1>
+                <span>
+                  <Image
+                    className='mx-auto footer-block-txt mb-2'
+                    src={logo}
+                    // onClick={() => {
+                    //   navigate('/');
+                    // }}
+                    style={{ height: '80px', objectFit: 'cover' }}
+                  />
+                </span>
+              </h1>
+              <p
+                className='text-center footer-block-txt my-2'
+                style={{ fontSize: '15px' }}
+              >
+                We offer specialized training in React.js, HTML, CSS, and
+                JavaScript, empowering learners with foundational skills for web
+                development and frontend careers.
+              </p>
+              <div className='ps-5'>
+                <div className='d-flex'>
+                  <img style={{ width: '25px' }} src={A} />
+                  <h6 className='mt-1 mx-3 footer-block-txt '>
+                    +91 9575212142
+                  </h6>
+                </div>
+                <div className='d-flex mt-2'>
+                  <img style={{ width: '25px' }} src={B} />
+                  <h6 className='text-center mt-1 mx-3 footer-block-txt '>
+                    info@grapheneinfotech.com
+                  </h6>
+                </div>
+              </div>
+            </div>
 
-   
+            <div className='my-5 footer-links'>
+              <h4
+                className=' text-start footer-block-txt pb-1 page-title'
+                style={{ fontSize: '22px' }}
+              >
+                QuickLinks
+              </h4>
+              <h5
+                className='text-start footer-block-txt'
+                style={{ fontSize: '16px' }}
+              >
+                About Us
+              </h5>
+              <h5
+                className='text-start footer-block-txt'
+                style={{ fontSize: '16px' }}
+              >
+                Contact Us
+              </h5>
+
+              {/* <h5
+                className='text-start footer-block-txt'
+                style={{ fontSize: '16px' }}
+              >
+                Product
+              </h5> */}
+              <h5
+                className='text-start footer-block-txt '
+                style={{ fontSize: '16px' }}
+              >
+                Blogs
+              </h5>
+            </div>
+            <div className='my-5 footer-links'>
+              <h4
+                className='text-start footer-block-txt page-title'
+                style={{ fontSize: '22px' }}
+              >
+                Help
+              </h4>
+              <h5
+                className='text-start footer-block-txt'
+                style={{ fontSize: '16px' }}
+              >
+                Payment Details
+              </h5>
+
+              <h5
+                className='text-start footer-block-txt'
+                style={{ fontSize: '16px' }}
+              >
+                FAQ's
+              </h5>
+            </div>
+            <div className='my-5 footer-links'>
+              <h4
+                className='text-start footer-block-txt pb-1 page-title'
+                style={{ fontSize: '22px' }}
+              >
+                Customer Policy
+              </h4>
+              <h5
+                className='text-start footer-block-txt'
+                style={{ fontSize: '16px' }}
+              >
+                Privacy Policy
+              </h5>
+              <h5
+                className='text-start footer-block-txt'
+                style={{ fontSize: '16px' }}
+              >
+                Terms & Conditions
+              </h5>
+            </div>
+          </div>
+        </div>
+        <h5 className='text-center pt-3 pb-3 footer-block-txt'>
+          @2024-2029 grapheneinfotech.com
+        </h5>
+      </div>
     </footer>
   );
 }
